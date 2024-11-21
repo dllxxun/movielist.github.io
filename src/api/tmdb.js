e9825e16dc3e669fd4a27cbddbfea4228a597d3b
[yeon] <[cyj0749@naver.com]>
2024-11-21 22:13:32 +0900

    router 문제 수정

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// API 키 확인
+console.log('API Key:', process.env.VUE_APP_TMDB_API_KEY);
+
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
@@ -9,10 +12,17 @@ const instance = axios.create({
 });
 
 export const tmdbApi = {
-    getNowPlaying: () => instance.get('/movie/now_playing'),
-    getPopular: () => instance.get('/movie/popular'),
-    getTopRated: () => instance.get('/movie/top_rated'),
-    searchMovies: (query) => instance.get('/search/movie', {
-        params: { query }
-    })
+    getPopular: () => {
+        console.log('API 요청 시작');
+        return instance.get('/movie/popular')
+            .then(response => {
+                console.log('API 응답:', response);
+                return response;
+            })
+            .catch(error => {
+                console.error('API 에러:', error);
+                throw error;
+            });
+    },
+    // ... 나머지 메서드들
 };
