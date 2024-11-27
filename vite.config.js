56b593d29d20d08582273f557a51e6d6b08465b2
dllxxun <cyj0749@naver.com>
2024-11-27 22:14:51 +0900

    배포 수정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const { defineConfig } = require('@vue/cli-service')
 
-export default defineConfig({
-  base: '/movielist.github.io/',
-  plugins: [vue()],
-  define: {
-    __VUE_OPTIONS_API__: true,
-    __VUE_PROD_DEVTOOLS__: false,
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+module.exports = defineConfig({
+  publicPath: '/',
+  outputDir: 'dist',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src')
+      }
+    }
+  },
+  devServer: {
+    historyApiFallback: true
   }
 })
