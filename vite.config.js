7c536a4848328f3f9e6c7b39588d3d96e8199741
dllxxun <cyj0749@naver.com>
2024-11-27 23:10:26 +0900

    배포 롤백

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-module.exports = defineConfig({
-  publicPath: '/',
-  outputDir: 'dist',
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': require('path').resolve(__dirname, 'src')
-      }
-    }
-  },
-  devServer: {
-    historyApiFallback: true
+export default defineConfig({
+  base: '/movielist.github.io/',
+  plugins: [vue()],
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
   }
 })
